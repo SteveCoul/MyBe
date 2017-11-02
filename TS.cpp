3c41fb45336733b27ebe9dd095a2266dab7ffc14
Steve Coul <stevecoul@icloud.com>
2017-11-02 09:01:48 -0500

    add stream to TS

--- TS.cpp
+++ TS.cpp
@@ -20,10 +20,27 @@ TS::TS( const void* data, size_t length ) {
 	
 	if ( length != 0 ) 
 		XLOG_WARNING( "Excess of data at end of stream" );
+
+	for ( int i = 0; i < 8192; i++ ) m_stream[i] = new Stream( &m_packets_by_pid[ i ] );
 }
 
 TS::~TS() {
 	for ( std::vector<TSPacket*>::const_iterator it = m_packets.begin(); it != m_packets.end(); ++it )
 		delete *it;
+	for ( int i = 0; i < 8192; i++ ) delete m_stream[i];
+}
+
+TS::Stream* TS::stream( int pid ) { 
+	if ( ( pid < 0 ) || ( pid > 8191 ) ) return NULL;
+	return m_stream[ pid ];
+}
+
+unsigned int TS::Stream::numPackets() {
+	return m_packets->size();
+}
+
+TSPacket* TS::Stream::packet( unsigned int offset ) {
+	if ( offset >= m_packets->size() ) return NULL;
+	return m_packets->at( offset );
 }
 
--- TS.hpp
+++ TS.hpp
@@ -9,11 +9,24 @@
 
 class TS {
 public:
+	class Stream {
+	friend class TS;
+	public:
+		unsigned int numPackets();
+		TSPacket* packet( unsigned int offset );
+	protected:
+		Stream( std::vector<TSPacket*>* p ) { m_packets = p; }
+	private:
+		std::vector<TSPacket*>*	m_packets;
+	};
+public:
 	TS( const void* data, size_t length );
 	~TS();
+	Stream* stream( int pid );
 private:
 	std::vector<TSPacket*>	m_packets;
 	std::vector<TSPacket*>	m_packets_by_pid[ 8192 ];
+	Stream*					m_stream[ 8192 ];
 };
 
 #endif
