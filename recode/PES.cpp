33f1d2a9b6d185ca21ada2d19162260e0893301a
Steve Coul <stevecoul@icloud.com>
2017-12-08 08:51:27 -0600

    some PES header decode

--- recode/PES.cpp
+++ recode/PES.cpp
@@ -1,4 +1,5 @@
 
+#include "Misc.hpp"
 #include "xlog.hpp"
 
 #include "PES.hpp"
@@ -6,7 +7,85 @@
 PES::PES( const uint8_t* ptr, size_t len )
 	: m_ptr( ptr )
 	, m_len( len )
+	, m_streamId( 0 )
+	, m_length( 0 )
+	, m_scrambling_control( 0 )
+	, m_priority( false )
+	, m_data_alignment( false )
+	, m_copyright( false )
+	, m_original( false )
+	, m_hasPTS( false )
+	, m_hasDTS( false )
+	, m_payload( NULL )
+	, m_payload_length( 0 )
+	, m_PTS( 0 )
+	, m_DTS( 0 )
 	{
+
+	// PES Header
+	
+	if ( len <= 6 ) {
+		XLOG_WARNING("packet not long enough");
+		return;
+	}
+	m_streamId = ptr[3];
+	m_length = ((ptr[4] << 8 ) | ptr[5] );
+
+	if ( m_streamId < 0xBD ) return;
+	if ( m_streamId == 0xBE ) return;
+	if ( m_streamId == 0xBF ) return;
+	if ( m_streamId > 0xEF ) return;
+
+	ptr += 6;
+	len -= 6;
+
+	// PES Extension
+
+	if ( len < 3 ) {
+		XLOG_WARNING("not enough extension data");
+		return;
+	}
+
+	m_scrambling_control = (ptr[0]>>4)&3;
+	m_priority = ( (ptr[0]&0x8) != 0 );
+	m_data_alignment = ( (ptr[0]&0x04) != 0 );
+	m_copyright = ( (ptr[0]&0x02) != 0 );
+	m_original = ( (ptr[0]&0x01) != 0 );
+
+	m_hasPTS = ( ( ptr[1] & 0x80 ) != 0 );
+	m_hasDTS = ( ( ptr[1] & 0x40 ) != 0 );
+
+//	bool has_escr = ( ( ptr[1] & 0x20 ) != 0 );
+//	bool has_rate = ( ( ptr[1] & 0x10 ) != 0 );
+//	bool dsm_trick_mode = ( ( ptr[1] & 0x08 ) != 0 );
+//	bool has_additional = ( ( ptr[1] & 0x04 ) != 0 );
+//	bool pes_crc_flag = ( ( ptr[1] & 0x02 ) != 0 );
+//	bool pes_extension_flag = ( ( ptr[1] & 0x01 ) != 0 );
+
+	unsigned int pes_header_length = ptr[2];
+
+	ptr += 3;
+	len -= 3;
+
+	if ( len < pes_header_length ) {
+		XLOG_WARNING("not enough header data" );
+		return;
+	}
+
+	if ( m_hasPTS ) {
+		m_PTS = Misc::mpegTimestampFromBytes( ptr );
+		if ( m_hasDTS ) {
+			m_DTS = Misc::mpegTimestampFromBytes( ptr+5 );
+		}
+	} else if ( m_hasDTS ) {
+		m_DTS = Misc::mpegTimestampFromBytes( ptr );
+	}
+
+	ptr += pes_header_length;
+	len -= pes_header_length;
+
+	m_payload = ptr;
+	m_payload_length = ( m_length == 0 ) ? len : m_length;
 }
 
 PES::~PES() {
--- recode/PES.hpp
+++ recode/PES.hpp
@@ -9,9 +9,35 @@ public:
 	PES( const uint8_t* ptr, size_t len );
 	~PES();
 	void dump();
+	bool checkStartCode() { return ( ( m_len > 3 ) && ( m_ptr[0] == 0x00 ) && ( m_ptr[1] == 0x00 ) && ( m_ptr[2] == 0x01 ) ); }
+	unsigned int streamId() { return m_streamId; }
+	unsigned int packetLength() { return m_length; };
+	unsigned int scramblingControl() { return m_scrambling_control; }
+	bool priority() { return m_priority; }
+	bool dataAligned() { return m_data_alignment; }
+	bool copyright(){ return m_copyright; }
+	bool original() { return m_original; }
+	bool hasPTS() { return m_hasPTS; }
+	bool hasDTS() { return m_hasDTS; }
+	const uint8_t* payload( size_t* p_size = NULL ) { if ( p_size ) p_size[0] = m_payload_length; return m_payload; }
+	unsigned long long PTS() { return m_PTS; }
+	unsigned long long DTS() { return m_DTS; }
 private:
 	const uint8_t*	m_ptr;
 	size_t			m_len;
+	unsigned int	m_streamId;
+	unsigned int	m_length;
+	unsigned int	m_scrambling_control;
+	bool			m_priority;
+	bool			m_data_alignment;
+	bool			m_copyright;	
+	bool			m_original;
+	bool			m_hasPTS;
+	bool			m_hasDTS;
+	const uint8_t*	m_payload;
+	size_t			m_payload_length;
+	unsigned long long	m_PTS;
+	unsigned long long	m_DTS;
 };
 
 #endif
