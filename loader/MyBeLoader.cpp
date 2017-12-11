15730be1777120af7fac548be99dec0c18635786
Steve Coul <stevecoul@icloud.com>
2017-12-11 08:21:25 -0600

    begin loader impl

--- loader/MyBeLoader.cpp
+++ loader/MyBeLoader.cpp
@@ -1,34 +1,92 @@
 #include <assert.h>
+#include <stdio.h>
 
 #include "MyBeLoader.hpp"
 
+class Impl {
+public:
+	class Session {
+	public:
+		Session() {
+			assert(0);
+		}
+
+		~Session() {
+			assert(0);
+		}
+
+		bool start() {
+			assert(0);
+			return false;
+		}
+
+		void forceEarly() {
+			assert(0);
+		}
+
+		void forceBetter() {
+			assert(0);
+		}
+
+		void forceBest() {
+			assert(0);
+		}
+	};
+public:
+	Impl() {
+	}
+
+	~Impl() {
+	}
+
+	Impl::Session* createSession( const char* url, MyBeLoader::Callback* callback, unsigned int timeout ) {
+		assert(0);
+		return NULL;
+	}
+private:
+
+};
+
+/* ************************************************************************************************************** *
+ *
+ * ************************************************************************************************************** */
+
 MyBeLoader::MyBeLoader() {
+	m_impl = (void*) new Impl();
 }
 
 MyBeLoader::~MyBeLoader() {
+	delete (Impl*) m_impl;
 }
 
 MyBeLoader::session_t MyBeLoader::createSession( const char* url, Callback* callback, unsigned int timeout ) {
-	assert(0);
+	Impl::Session* s = ((Impl*)m_impl)->createSession( url, callback, timeout );
+	return (session_t)s;
 }
 
 bool MyBeLoader::commitSession( session_t session ) {
-	assert(0);
+	Impl::Session* s = (Impl::Session*)session;
+	if ( s->start() == false ) {
+		delete s;
+		return false;
+	}
+	return true;
 }
 
 bool MyBeLoader::abortSession( session_t session ) {
-	assert(0);
+	delete (Impl::Session*)session;
+	return true;
 }
 
 void MyBeLoader::forceSessionEarly( session_t session ) {
-	assert(0);
+	((Impl::Session*)session)->forceEarly();
 }
 
 void MyBeLoader::forceSessionBetter( session_t session ) {
-	assert(0);
+	((Impl::Session*)session)->forceBetter();
 }
 
 void MyBeLoader::forceSessionBest( session_t session ) {
-	assert(0);
+	((Impl::Session*)session)->forceBest();
 }
 
