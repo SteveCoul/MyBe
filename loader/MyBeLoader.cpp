83807e7cf9918a979edb00d62699edf5ecd1419d
Steve Coul <stevecoul@icloud.com>
2017-12-11 08:12:00 -0600

    stub loader library API

--- loader/MyBeLoader.cpp
+++ loader/MyBeLoader.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 
 #include "MyBeLoader.hpp"
 
@@ -5,5 +6,29 @@ MyBeLoader::MyBeLoader() {
 }
 
 MyBeLoader::~MyBeLoader() {
+}
+
+MyBeLoader::session_t MyBeLoader::createSession( const char* url, Callback* callback, unsigned int timeout ) {
+	assert(0);
+}
+
+bool MyBeLoader::commitSession( session_t session ) {
+	assert(0);
+}
+
+bool MyBeLoader::abortSession( session_t session ) {
+	assert(0);
+}
+
+void MyBeLoader::forceSessionEarly( session_t session ) {
+	assert(0);
+}
+
+void MyBeLoader::forceSessionBetter( session_t session ) {
+	assert(0);
+}
+
+void MyBeLoader::forceSessionBest( session_t session ) {
+	assert(0);
 }
 
