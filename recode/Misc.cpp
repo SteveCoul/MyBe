4949a613e1e305d47e3f91eccd576d0e5b6f9f1a
Steve Coul <stevecoul@icloud.com>
2017-12-26 08:22:39 -0600

    PES generation can now handle PTS or PTS+DTS

--- recode/Misc.cpp
+++ recode/Misc.cpp
@@ -59,18 +59,28 @@ std::vector<uint8_t>* Misc::makeUnboundPESSegment( const uint8_t* ptr, size_t le
 		rc->push_back( m_stream_id );
 		rc->push_back( 0x00 ); rc->push_back( 0x00 );		/* size = 0 */
 		rc->push_back( 0xC0 | 0x00 | 0x00 | 0x04 | 0x00 | 0x00 );	/* mpeg2, non-scrambled, non-priorty, aligned, nocopyright, original */
-		rc->push_back( 0xC0 | 0x00 | 0x00 | 0x00 | 0x00 | 0x00 | 0x00 );	/* PTS+DTS, no-escr, no-esrate, no-dsm, no-additional, no-crc, no-extension */
-		rc->push_back( 0x0A );								/* header-size */
-		rc->push_back( 0x30 | ( ( ( pts >> 30 ) & 0x07 ) << 1 ) | 0x01 );
+
+		if ( dts != 0xFFFFFFFFFFFFFFFFUL ) {
+			rc->push_back( 0xC0 | 0x00 | 0x00 | 0x00 | 0x00 | 0x00 | 0x00 );	/* PTS+DTS, no-escr, no-esrate, no-dsm, no-additional, no-crc, no-extension */
+			rc->push_back( 0x0A );								/* header-size */
+			rc->push_back( 0x30 | ( ( ( pts >> 30 ) & 0x07 ) << 1 ) | 0x01 );
+		} else {
+			rc->push_back( 0x80 | 0x00 | 0x00 | 0x00 | 0x00 | 0x00 | 0x00 );	/* PTS+DTS, no-escr, no-esrate, no-dsm, no-additional, no-crc, no-extension */
+			rc->push_back( 0x05 );								/* header-size */
+			rc->push_back( 0x20 | ( ( ( pts >> 30 ) & 0x07 ) << 1 ) | 0x01 );
+		}
+
 		rc->push_back( ( pts >> 23 ) & 255 );
 		rc->push_back( ( ( pts >> 15 ) & 0x7F ) << 1 );
 		rc->push_back( ( pts >> 7 ) & 255 );
 		rc->push_back( ( ( pts & 0x7F ) << 1 ) | 0x01 );
-		rc->push_back( 0x01 | ( ( ( dts >> 30 ) & 0x07 ) << 1 ) | 0x01 );
-		rc->push_back( ( dts >> 23 ) & 255 );
-		rc->push_back( ( ( dts >> 15 ) & 0x7F ) << 1 );
-		rc->push_back( ( dts >> 7 ) & 255 );
-		rc->push_back( ( ( dts & 0x7F ) << 1 ) | 0x01 );
+		if ( dts != 0xFFFFFFFFFFFFFFFFUL ) {
+			rc->push_back( 0x01 | ( ( ( dts >> 30 ) & 0x07 ) << 1 ) | 0x01 );
+			rc->push_back( ( dts >> 23 ) & 255 );
+			rc->push_back( ( ( dts >> 15 ) & 0x7F ) << 1 );
+			rc->push_back( ( dts >> 7 ) & 255 );
+			rc->push_back( ( ( dts & 0x7F ) << 1 ) | 0x01 );
+		}
 		rc->insert( rc->end(), ptr, ptr+len );
 	}
 	return rc;
