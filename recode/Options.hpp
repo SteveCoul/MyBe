d8252c52b20a7c4f451a3ba8e96068f8138c8926
Steve Coul <stevecoul@icloud.com>
2017-12-28 08:05:56 -0600

    doxygen options

--- recode/Options.hpp
+++ recode/Options.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 
+/// Command line option parsing.
 class Options {
 public:
 
@@ -15,17 +16,38 @@ public:
 		erase();
 	}
 
+	/// \brief		Process incoming command line arguments and configure.
+	/// \param[in]	argc	Count of arguments.
+	/// \param[in]	argv	Incoming arguments.
+	/// \return 0 on success.
 	int process( int argc, char** argv ) {
 		erase();
 		setDefault();
 		return parse( argc, argv );
 	}
 
+	/// \brief Return source stream filename
+	/// \return value.
 	std::string source() { return m_source_filename; }
+
+	/// \brief Return destination filename
+	/// \return value.
 	std::string dest() { return m_dest_filename; }
+
+	/// \brief Check to see if intermediate video streams should be saved.
+	/// \return value.
 	bool saveVideos() { return m_save_videos; }
+
+	/// \brief Check to see if videos should be decoded and have frame files dumped.
+	/// \return value.
 	bool decodeVideos() { return m_decode_videos; }
+
+	/// \brief Return how many frames should be duplicated in alternate stream.
+	/// \return value.
 	int frames() { return m_frames; }
+
+	/// \brief Return alternate video encode rate.
+	/// \return value.
 	int rate() { return m_rate; }
 
 private:
